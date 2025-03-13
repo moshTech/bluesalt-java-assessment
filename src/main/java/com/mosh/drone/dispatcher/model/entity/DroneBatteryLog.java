ce4809330f2fc6411f925b62b77abf54bbcd62f7
Moshood Oyetola <mosh@digicoreltd.com>
2025-03-13 11:43:29 +0100

    Added DroneBatteryLog table and repository

---
+++ src/main/java/com/mosh/drone/dispatcher/model/entity/DroneBatteryLog.java
@@ -0,0 +1,33 @@
+package com.mosh.drone.dispatcher.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+/**
+ * @author mosh
+ * @role software engineer
+ * @createdOn 13 Thu Mar, 2025
+ */
+
+@Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
+@Table(name = "battery_log")
+@EqualsAndHashCode(callSuper = false)
+@Where(clause = "deleted = false")
+@SQLDelete(sql = "update battery_log set deleted = true where id=?")
+@Builder
+public class DroneBatteryLog extends BaseEntity{
+
+    private String droneId;
+
+    private int batteryLevel;
+}
---
+++ src/main/java/com/mosh/drone/dispatcher/repository/DroneBatteryLogRepository.java
@@ -0,0 +1,15 @@
+package com.mosh.drone.dispatcher.repository;
+
+import com.mosh.drone.dispatcher.model.entity.DroneBatteryLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author mosh
+ * @role software engineer
+ * @createdOn 13 Thu Mar, 2025
+ */
+
+@Repository
+public interface DroneBatteryLogRepository extends JpaRepository<DroneBatteryLog, String> {
+}
